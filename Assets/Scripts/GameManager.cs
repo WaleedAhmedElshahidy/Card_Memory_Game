80864515a9efc009b3db51f02b2ed20de10f37e4
WaleedElshahidy-Thaka <106826650+WaleedAhmedElshahidy@users.noreply.github.com>
2026-03-12 18:32:15 +0200

    Finished Game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,146 +1,276 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    public Sprite bgBtn;
+    // ---- Events ----
+    public static event Action<int> OnScoreChanged;
+    public static event Action<int> OnGuessChanged;
+    public static event Action OnGameFinished;
 
-    public List<Button> btns = new List<Button>();
-    public Sprite[] cardsSprites;
-    public List<Sprite> cards = new List<Sprite>();
+    public UnityEvent OnGameStarted;
 
-    private bool firstGuess, secondGuess;
-    private int countGuesses;
-    private int countCorrectGuesses;
-    private int gameGuesses;
+    // ---- Serialized Fields ----
+    [SerializeField] private Card cardPrefab;
+    [SerializeField] private RectTransform gridTransform;
+    [SerializeField] private GridLayoutConfig gridConfigCS;
+    [SerializeField] private int totalCards = 16;
 
-    private int firstGuessIndex, secondGuessIndex;
+    // ---- Private Fields ----
+    private Sprite[] cardsSprites;
+    private List<Sprite> spritePairs;
+    private List<Card> spawnedCards = new List<Card>();
+    private List<int> spriteIndices = new List<int>();
 
-    private string firstGuessPuzzle, secondGuessPuzzle;
+    private Card firstSelected;
+    private Card secondSelected;
 
+    private int score = 0;
+    private int matchedPairs = 0;
+    private int totalPairs;
+    private int countGuesses = 0;
+
+    // ---- Unity Methods ----
     private void Awake()
     {
         cardsSprites = Resources.LoadAll<Sprite>("YoGiOhCards");
-    }
-    private void Start()
-    {
-        GetButtons();
-        AddListeners();
-        AddGamePuzzles();
-        gameGuesses = cards.Count / 2;
+
+        if (cardsSprites == null || cardsSprites.Length == 0)
+            Debug.LogError("No sprites loaded! Check: Resources/YoGiOhCards");
     }
 
-    void GetButtons()
-    {
-        GameObject[] BtnsObjects = GameObject.FindGameObjectsWithTag("PuzzleBtn");
+    //private void Start()
+    //{
+    //    if (SaveSystem.HasSave())
+    //        LoadGame();
+    //    else
+    //        StartNewGame();
+    //}
 
-        for (int i = 0; i < BtnsObjects.Length; i++)
-        {
-            btns.Add(BtnsObjects[i].GetComponent<Button>());
-            btns[i].image.sprite = bgBtn;
-            BtnsObjects[i].name = i.ToString();
-        }
+    // ---- New Game ----
+    public void StartNewGame()
+    {
+        score = 0;
+        matchedPairs = 0;
+        countGuesses = 0;
+        totalPairs = totalCards / 2;
+
+        PrepareSprites();
+
+        // force canvas update so container has correct size
+        Canvas.ForceUpdateCanvases();
+
+        gridConfigCS.SetupGrid(totalCards, gridTransform);
+        CreateCards();
     }
 
-    void AddGamePuzzles()
+    public void StartNewGame(int selectedCards)
     {
-        int looper = btns.Count;
-        int index = 0;
-        int pairsNeeded = looper / 2;
-
-        for (int i = 0; i < looper; i++)
-        {
-            if (index == pairsNeeded) index = 0;
-            cards.Add(cardsSprites[index]);
-            index++;
-        }
-
-        // Shuffle the cards list
-        for (int i = 0; i < cards.Count; i++)
-        {
-            Sprite temp = cards[i];
-            int randomIndex = Random.Range(i, cards.Count);
-            cards[i] = cards[randomIndex];
-            cards[randomIndex] = temp;
-        }
-    }
-    void AddListeners()
-    {
-        foreach (Button btn in btns)
-        {
-            btn.onClick.AddListener(() => PickAPuzzle());
-        }
+        totalCards = selectedCards;
+        StartNewGame(); 
     }
 
-    public void PickAPuzzle()
+    private void PrepareSprites()
     {
-        if (!firstGuess)
+        spritePairs = new List<Sprite>();
+        spriteIndices = new List<int>();
+
+        for (int i = 0; i < totalPairs; i++)
         {
-            firstGuess = true;
-
-            firstGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
-
-            firstGuessPuzzle = cards[firstGuessIndex].name;
-
-            btns[firstGuessIndex].image.sprite = cards[firstGuessIndex];
-
-
-
+            spritePairs.Add(cardsSprites[i]);
+            spritePairs.Add(cardsSprites[i]);
+            spriteIndices.Add(i);
+            spriteIndices.Add(i);
         }
-        else if (!secondGuess)
+
+        ShuffleSprites();
+    }
+
+    private void ShuffleSprites()
+    {
+        for (int i = spritePairs.Count - 1; i > 0; i--)
         {
-            secondGuess = true;
+            int randomIndex = UnityEngine.Random.Range(0, i + 1);
 
-            secondGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            Sprite tempSprite = spritePairs[i];
+            spritePairs[i] = spritePairs[randomIndex];
+            spritePairs[randomIndex] = tempSprite;
 
-            secondGuessPuzzle = cards[secondGuessIndex].name;
-
-
-
-            btns[secondGuessIndex].image.sprite = cards[secondGuessIndex];
-            countGuesses++;
-            StartCoroutine(CheckIfMatch());
-
+            int tempIndex = spriteIndices[i];
+            spriteIndices[i] = spriteIndices[randomIndex];
+            spriteIndices[randomIndex] = tempIndex;
         }
     }
 
-    IEnumerator CheckIfMatch()
+    private void CreateCards()
     {
-        yield return new WaitForSeconds(1);
+        foreach (Transform child in gridTransform)
+            Destroy(child.gameObject);
 
-        if (firstGuessPuzzle == secondGuessPuzzle)
+        spawnedCards.Clear();
+
+        for (int i = 0; i < spritePairs.Count; i++)
         {
-            yield return new WaitForSeconds(.5f);
-            btns[firstGuessIndex].image.color = new Color(0,0,0,0);
-            btns[secondGuessIndex].image.color = new Color(0,0,0,0);
-
-
-
-
-            CheckIfGameFinished();
-        }else
-        {
-            yield return new WaitForSeconds(.5f);
-
-
-            btns[firstGuessIndex].image.sprite = bgBtn;
-            btns[secondGuessIndex].image.sprite = bgBtn;
+            Card card = Instantiate(cardPrefab, gridTransform);
+            card.name = i.ToString();
+            card.SetIconSprite(spritePairs[i]);
+            card.controller = this;
+            spawnedCards.Add(card);
         }
-
-        yield return new WaitForSeconds(.5f);
-
-        firstGuess = secondGuess = false;
+        OnGameStarted?.Invoke();
     }
 
-
-    private void CheckIfGameFinished()
+    public void SetSelected(Card card)
     {
-        countCorrectGuesses++;
-        if (countCorrectGuesses == gameGuesses)
+        if (card.isSelected) return;
+        if (card.isMatched) return;
+
+        // if two cards already pending hide them immediately before continuing
+        if (firstSelected != null && secondSelected != null)
         {
-            // game finished
+            StopAllCoroutines();
+
+            if (!firstSelected.isMatched) firstSelected.Hide();
+            if (!secondSelected.isMatched) secondSelected.Hide();
+
+            firstSelected = null;
+            secondSelected = null;
         }
+
+        AudioManager.Instance.PlayFlip();
+        card.Show();
+
+        if (firstSelected == null)
+        {
+            firstSelected = card;
+            return;
+        }
+
+        secondSelected = card;
+        countGuesses++;
+
+        OnGuessChanged?.Invoke(countGuesses);
+        StartCoroutine(CheckMatching(firstSelected, secondSelected));
+    }
+
+    private IEnumerator CheckMatching(Card a, Card b)
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        if (a.iconSprite == b.iconSprite)
+        {
+            a.isMatched = true;
+            b.isMatched = true;
+
+            matchedPairs++;
+            score += 10;
+
+            AudioManager.Instance.PlayMatch();
+            OnScoreChanged?.Invoke(score);
+
+            SaveGame();
+
+            if (matchedPairs >= totalPairs)
+            {
+                SaveSystem.DeleteSave();
+                AudioManager.Instance.PlayGameOver();
+                OnGameFinished?.Invoke();
+            }
+        }
+        else
+        {
+            AudioManager.Instance.PlayMismatch();
+
+            yield return new WaitForSeconds(0.3f);
+
+            // only hide if they haven't been clicked again
+            if (!a.isMatched) a.Hide();
+            if (!b.isMatched) b.Hide();
+        }
+
+        firstSelected = null;
+        secondSelected = null;
+    }
+
+    // ---- Save / Load ----
+    private void SaveGame()
+    {
+        GameSaveData data = new GameSaveData
+        {
+            score = this.score,
+            matchedPairs = this.matchedPairs,
+            countGuesses = this.countGuesses,
+            totalCards = this.totalCards,
+            spriteIndices = spriteIndices.ToArray(),
+            matchedCards = new bool[spawnedCards.Count]
+        };
+
+        for (int i = 0; i < spawnedCards.Count; i++)
+            data.matchedCards[i] = spawnedCards[i].isMatched;
+
+        SaveSystem.Save(data);
+    }
+
+    public void LoadGame()
+    {
+        GameSaveData data = SaveSystem.Load();
+
+        if (data == null)
+        {
+            Debug.LogWarning("No save data found!");
+            return; 
+        }
+
+        score = data.score;
+        matchedPairs = data.matchedPairs;
+        countGuesses = data.countGuesses;
+        totalCards = data.totalCards;
+        totalPairs = totalCards / 2;
+
+        // rebuild sprite pairs from saved indices
+        spritePairs = new List<Sprite>();
+        spriteIndices = new List<int>(data.spriteIndices);
+
+        foreach (int idx in spriteIndices)
+            spritePairs.Add(cardsSprites[idx]);
+
+        Canvas.ForceUpdateCanvases();
+        gridConfigCS.SetupGrid(totalCards, gridTransform);
+        CreateCards();
+
+        // restore matched state
+        for (int i = 0; i < spawnedCards.Count; i++)
+        {
+            if (data.matchedCards[i])
+            {
+                spawnedCards[i].isMatched = true;
+                spawnedCards[i].isSelected = true;
+                spawnedCards[i].Show();
+            }
+        }
+
+        OnScoreChanged?.Invoke(score);
+        OnGuessChanged?.Invoke(countGuesses);
+    }
+
+    // ---- Public Helpers ----
+    public void SetTotalCards(int count)
+    {
+        totalCards = count;
+    }
+
+    public void RestartGame()
+    {
+        SaveSystem.DeleteSave();
+        StartNewGame();
+        UIManager.Instance.scoreText.text = "Score: 0";
+        UIManager.Instance.guessesText.text = "Guesses: 0";
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
